d2a8f3a51e7cc7851876fc18cf4931ea359b8ca4
Leo Guo <shxguozhq@qq.com>
2023-04-28 22:42:54 +0800

    新建两个基础框架并配置路由

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,12 +2,13 @@ import {
   createBrowserRouter,
 } from 'react-router-dom'
 import { NotFoundPage } from '../components/NotFoundPage'
+import { MainLayout } from '../layouts/MainLayout'
 import { welcomeRoutes } from './welcomeRoutes'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <MainLayout />,
     errorElement: <NotFoundPage />,
     children: [
       welcomeRoutes,
--- src/routes/welcomeRoutes.tsx
+++ src/routes/welcomeRoutes.tsx
@@ -1,8 +1,8 @@
-import { Outlet } from 'react-router-dom'
+import { WelcomeLayout } from '../layouts/WelcomeLayout'
 
 export const welcomeRoutes = {
   path: 'welcome',
-  element: <div><Outlet /></div>,
+  element: <WelcomeLayout />,
   children: [
     { path: '1', element: <div>1</div> },
     { path: '2', element: <div>2</div> },

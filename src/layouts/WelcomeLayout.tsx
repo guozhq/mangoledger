0c6c473511e53a0c66088b90ac0071eeee0ba698
Leo Guo <shxguozhq@qq.com>
2023-04-29 22:41:17 +0800

    解决首页动画bug和map内存泄漏bug

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,21 +1,26 @@
 import { animated, useTransition } from '@react-spring/web'
 import type { ReactNode } from 'react'
+import { useRef } from 'react'
 import { useLocation, useOutlet } from 'react-router-dom'
 
-const map: Record<string, ReactNode> = {}
 export const WelcomeLayout: React.FC = () => {
+  const map = useRef<Record<string, ReactNode>>({})
   const location = useLocation()
   const outlet = useOutlet()
-  map[location.pathname] = outlet
+  map.current[location.pathname] = outlet
   const transitions = useTransition(location.pathname, {
-    from: { transform: 'translateX(100%)' },
+    from: {
+      transform: location.pathname === '/welcome/1'
+        ? 'translateX(100%)'
+        : 'translateX(0)',
+    },
     enter: { transform: 'translateX(0)' },
     leave: { transform: 'translateX(-100%)' },
     config: { duration: 1000 },
   })
   return transitions((style, pathname) => {
     return <animated.div key={pathname} style={style}>
-          {map[pathname]}
+          {map.current[pathname]}
         </animated.div>
   })
 }

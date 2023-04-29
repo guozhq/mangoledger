1d4f5d16229f71d3bc6dc0be58b608ec7d40fcb7
Leo Guo <shxguozhq@qq.com>
2023-04-29 10:53:55 +0800

    初始化路由动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,5 +1,21 @@
-import { Outlet } from 'react-router-dom'
+import { animated, useTransition } from '@react-spring/web'
+import type { ReactNode } from 'react'
+import { useLocation, useOutlet } from 'react-router-dom'
 
+const map: Record<string, ReactNode> = {}
 export const WelcomeLayout: React.FC = () => {
-  return (<div><Outlet /></div>)
+  const location = useLocation()
+  const outlet = useOutlet()
+  map[location.pathname] = outlet
+  const transitions = useTransition(location.pathname, {
+    from: { transform: 'translateX(100%)' },
+    enter: { transform: 'translateX(0)' },
+    leave: { transform: 'translateX(-100%)' },
+    config: { duration: 1000 },
+  })
+  return transitions((style, pathname) => {
+    return <animated.div key={pathname} style={style}>
+          {map[pathname]}
+        </animated.div>
+  })
 }

3e03580073c7e81339d6d5b294027493024fc899
Leo Guo <shxguozhq@qq.com>
2023-04-30 11:29:05 +0800

    解决动画播放失败问题

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -25,23 +25,22 @@ export const WelcomeLayout: React.FC = () => {
     leave: { transform: 'translateX(-100%)' },
     config: { duration: 1000 },
   })
-  return transitions((style, pathname) => {
-    return (
+  return (
       <div className="bg-#5e34bf" h-screen flex flex-col items-stretch pb-16px>
         <header shrink-0 text-center >
           <img src={ logo } w-64px/>
           <h1 color="#D4D4EE" text-32px>React 记账</h1>
         </header>
         <main bg-white m-16px rounded-8px flex justify-center items-center text-center shrink-1 grow-1>
+        {transitions((style, pathname) =>
           <animated.div key={pathname} style={style}>
             {map.current[pathname]}
-          </animated.div>
+          </animated.div>)}
         </main>
         <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-rows-1>
           <Link to={linkMap[location.pathname]} style={{ gridArea: '1 / 2 / 2 / 3' }}>下一页</Link>
           <Link to='/welcome/xxx' style={{ gridArea: '1 / 3 / 2 / 4' }}>跳过</Link>
         </footer>
       </div>
-    )
-  })
+  )
 }

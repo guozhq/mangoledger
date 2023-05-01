738de57d69e694379ab485a32d5832ee1660aff9
Leo Guo <shxguozhq@qq.com>
2023-05-01 16:50:59 +0800

    滑动可进入下一页了

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,8 +1,9 @@
 import { animated, useTransition } from '@react-spring/web'
 import type { ReactNode } from 'react'
-import { useRef, useState } from 'react'
-import { Link, useLocation, useOutlet } from 'react-router-dom'
+import { useEffect, useRef, useState } from 'react'
+import { Link, useLocation, useNavigate, useOutlet } from 'react-router-dom'
 import logo from '../assets/images/mangosteen.svg'
+import { useSwipe } from '../hooks/useSwipe'
 
 const linkMap: Record<string, string> = {
   '/welcome/1': '/welcome/2',
@@ -11,6 +12,7 @@ const linkMap: Record<string, string> = {
   '/welcome/4': '/welcome/1',
 }
 export const WelcomeLayout: React.FC = () => {
+  const animating = useRef(false)
   const map = useRef<Record<string, ReactNode>>({})
   const location = useLocation()
   const outlet = useOutlet()
@@ -29,17 +31,27 @@ export const WelcomeLayout: React.FC = () => {
       setextraStyle({ position: 'absolute' })
     },
     onRest: () => {
+      animating.current = false
       setextraStyle({ position: 'relative' })
     },
   })
-
+  const main = useRef<HTMLElement>(null)
+  const { direction } = useSwipe(main, { onTouchStart: e => e.preventDefault() })
+  const nav = useNavigate()
+  useEffect(() => {
+    if (direction === 'left') {
+      if (animating.current) { return }
+      animating.current = true
+      nav(linkMap[location.pathname])
+    }
+  }, [direction, location.pathname, linkMap])
   return (
       <div className="bg-#5e34bf" h-screen flex flex-col items-stretch pb-16px>
         <header shrink-0 text-center >
           <img src={ logo } w-64px h-69px />
           <h1 color="#D4D4EE" text-32px>React</h1>
         </header>
-        <main shrink-1 grow-1 relative m-16px>
+        <main shrink-1 grow-1 relative m-16px ref={main}>
         {transitions((style, pathname) =>
           <animated.div key={pathname} style={{ ...style, ...extraStyle }} w-full h-full p-16px flex>
             <div bg-white grow-1 flex justify-center items-center rounded-8px>

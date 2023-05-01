0a33d6948f93b91a55b1e10445a9ed39861f8a67
Leo Guo <shxguozhq@qq.com>
2023-05-01 18:15:50 +0800

    优化目录结构

--- src/layouts/MainLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-export const MainLayout: React.FC = () => {
-  const hasRead = localStorage.getItem('hasRead')
-  if (hasRead === 'yes') {
-    return <Navigate to='/home'/>
-  }
-  return (<div><Outlet /></div>)
-}

34a3cd5f93df3312b6aee5db074ebd56ac33f235
Leo Guo <shxguozhq@qq.com>
2023-05-04 18:16:25 +0800

    创建 ItemsPage 页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -8,6 +8,7 @@ import { Welcome2 } from '../pages/Welcome2'
 import { Welcome3 } from '../pages/Welcome3'
 import { Welcome4 } from '../pages/Welcome4'
 import { Home } from '../pages/Home'
+import { ItemsPages } from '../pages/ItemsPage'
 
 export const router = createBrowserRouter([
   {
@@ -30,6 +31,6 @@ export const router = createBrowserRouter([
   },
   {
     path: 'items',
-    element: <div>items</div>
+    element: <ItemsPages />
   }
 ])

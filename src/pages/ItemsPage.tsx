15e48a889f17c0344290653448a54034342eb704
Leo Guo <shxguozhq@qq.com>
2023-05-10 16:19:48 +0800

    完成滑动菜单动画

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -46,7 +46,7 @@ export const ItemsPages: React.FC = () => {
       <ItemsSummary />
       <ItemsList items={items}/>
       <AddItemFloatButton />
-      {visible ? <TopMenu onClickMask={() => { setVisible(false) }}/> : null}
+      <TopMenu visible={visible} onClickMask={() => { setVisible(false) }}/>
     </div>
   )
 }

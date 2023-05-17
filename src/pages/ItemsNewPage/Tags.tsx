8785f00a4aa1698098a5289c82f8391e95429145
Leo Guo <shxguozhq@qq.com>
2023-05-17 22:32:32 +0800

    消除li没有添加key属性的bug

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -15,8 +15,8 @@ export const Tags: React.FC<Props> =(props)=>{
           flex justify-center items-center text-24px color="#8F4CD7"
           ><Icon name="add" className="text-32px" /></span>
       </li>
-      {tags.map(tag =>
-      <li w-48px flex justify-center items-center flex-col gap-y-8px>
+      {tags.map((tag, index) =>
+      <li key={index} w-48px flex justify-center items-center flex-col gap-y-8px>
         <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
               flex justify-center items-center text-24px b-1 b="#8F4CD7">😶</span>
         <span text-12px color="#666">打车</span>

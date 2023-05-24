6693912b685e17f89c3c7b11c68dcd7eb18c509b
Leo Guo <shxguozhq@qq.com>
2023-05-24 10:55:37 +0800

    基本完成折线图样式

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,30 +1,65 @@
 import { useEffect, useRef } from 'react'
 import * as echarts from 'echarts'
-
-export const LineChart: React.FC = () => {
+type Props = {
+  className?: string
+  items?:{x:number|string; y:number}[]
+}
+export const LineChart: React.FC<Props> = (props) => {
+  const {className,items} = props
+  const xItems = items?.map(item => item.x)
+  const yItems = items?.map(item => item.y)
   const div = useRef<HTMLDivElement>(null)
   useEffect(() => {
     if (!div.current) { return }
     const myChart = echarts.init(div.current)
     const option = {
+      tooltip: {
+        trigger: 'axis',
+        show: true,
+        formatter: ([{ axisValue, data }]: any) => {
+          const parts = axisValue.split('-')
+          const label = `${parts[0]}年${parts[1]}月${parts[2]}日`
+          const value = data === null ? '无数据' : `${data}元`
+          return `${label}<br/><div style="text-align:right;">${value}</div>`
+        }
+      },
+      grid:{
+        left:16,
+        top:8,
+        bottom:24,
+        right:16,
+      },
       xAxis: {
         type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        data: xItems,
+        axisLabel: {
+          formatter: (label: string) =>
+            label.slice(label.indexOf('-') + 1)
+        },
       },
       yAxis: {
-        type: 'value'
+        type: 'value',
+        axisLabel: {
+          show: false
+        }
       },
       series: [
         {
-          data: [150, 230, 224, 218, 135, 147, 260],
-          type: 'line'
+          data: yItems,
+          type: 'line',
+          emphasis: {
+            itemStyle: {
+              color: 'green',
+            }
+          },
         }
-      ]
+      ],
+      
     }
 
     myChart.setOption(option)
   })
   return (
-    <div ref={div} h-400px></div>
+    <div ref={div} h-400px className={className}></div>
   )
 }

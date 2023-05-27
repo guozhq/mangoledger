0b25a9140808ca5d666d75525726e42fb7eb5154
Leo Guo <shxguozhq@qq.com>
2023-05-28 01:57:45 +0800

    将 jwt 放入 localStorage，然后在每次请求时将其放入请求头中

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,12 +1,19 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 axios.defaults.baseURL = isDev ? '/' : 'http:1.1.1.1:8080/api/v1'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.timeout = 1000
 
+axios.interceptors.request.use((config)=>{
+  const jwt = localStorage.getItem('jwt') || ''
+  config.headers = config.headers || {}
+  if (jwt) {config.headers.Authorization = `Bearer ${localStorage.getItem('jwt')}`}
+  return config
+})
+
 export const ajax = {
-  get: <T>(path: string) => {
-    return axios.get<T>(path)
+  get: <T>(path: string,config?: AxiosRequestConfig<any>) => {
+    return axios.get<T>(path,config)
   },
   post: <T>(path: string, data: JSONValue) => {
     return axios.post<T>(path, data)

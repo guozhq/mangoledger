cc50159485ab00ac787171ec05272b2248363487
Leo Guo <shxguozhq@qq.com>
2023-06-04 21:13:29 +0800

    items 不再是写死的，而是从后端获取的

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -23,6 +23,9 @@ export class Time{
   get lastDayOfMonth() {
     return new Time(new Date(this.year, this.month - 1 + 1, 0))
   }
+  get firstDayOfMonth(){
+    return new Time(new Date(this.year, this.month - 1, 1))
+  }
   /**
    * 格式化输出
    * @param pattern 目前只支持 yyyy MM dd HH mm ss fff

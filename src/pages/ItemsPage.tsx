ad8b3c8b99497e22cc66130c37de7e556f56ee03
Leo Guo <shxguozhq@qq.com>
2023-06-06 09:05:46 +0800

    重构TimeRange类型

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -10,9 +10,14 @@ import { Icon } from '../components/Icon'
 import { ItemsList } from './ItemsPage/ItemsList'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
 import { timeRangeToStartAndEnd } from '../lib/timeRangeToStartAndEnd'
+import { time } from '../lib/time'
 
 export const ItemsPages: React.FC = () => {
-  const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
+  const [timeRange, setTimeRange] = useState<TimeRange>({
+    name: 'thisMonth',
+    start: time().firstDayOfMonth,
+    end: time().lastDayOfMonth.add(1, 'day')
+  })
   const { visible, setVisible } = useMenuStore()
   const {start, end} = timeRangeToStartAndEnd(timeRange)
   return (
